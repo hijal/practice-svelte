cf691cdb4b44353409611b38d3acf1ee295520e2
hijal <hijikeshhijal@gmail.com>
2024-03-24 13:58:06 +0600

    all about svelte props

---
+++ props/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Nested from './lib/Nested.svelte';
+  import Info from './lib/Info.svelte';
+
+  const pkg = {
+    name: 'Svelte',
+    version: '4.2.12',
+    speed: 'blazing',
+    website: 'https://svelte.dev'
+  };
+</script>
+
+<main>
+  <Nested answer={Math.floor(Math.random() * 100 + 1)} />
+  <Nested />
+  <Info {...pkg} />
+</main>
+
+<style>
+</style>
---
+++ props/src/lib/Nested.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let answer = 0;
+</script>
+
+<p>
+  The answer is: <strong>{answer}</strong>
+</p>
